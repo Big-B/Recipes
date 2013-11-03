6d9fd869256f19d3f6370a35d7a34e2f14ee80dc
Brandon Mittman <brandonmittman@gmail.com>
2013-11-02 18:49:31 -0700

    Bug/syntax fix

--- GroceryList.rb
+++ GroceryList.rb
@@ -12,7 +12,7 @@ class GroceryList
     # This means combining items that are the same and combining their
     # amounts.
     def addRecipe(recipe)
-        @list << recipe
+        @list[recipe.name] = recipe
         if(!@recipes.include?(recipe))
             @recipes << recipe
         end
@@ -20,23 +20,31 @@ class GroceryList
 
     # Remove a recipe from the grocery list
     def removeRecipe(recipe)
-        @list.delete(recipe)
+        @list.delete(recipe.name)
     end
 
 end
 
 beef = Item.new("Beef", 1)
+chicken = Item.new("Chicken", 3)
+cheese = Item.new("Swiss", 2)
 spaghetti = Item.new("Spaghetti Noodles", 1)
 sauce = Item.new("Ragu", 1)
 
+chicken_recipe = Recipe.new("Chicken")
+chicken_recipe.addItem(chicken)
+chicken_recipe.addItem(cheese)
 spaghetti_recipe = Recipe.new("Spaghetti")
 spaghetti_recipe.addItem(beef)
 spaghetti_recipe.addItem(spaghetti)
 spaghetti_recipe.addItem(sauce)
 
 spaghetti_recipe.print()
+grocery = GroceryList.new()
+grocery.addRecipe(chicken_recipe)
+grocery.addRecipe(spaghetti_recipe)
 
-file = File.open(spaghetti_recipe.name, 'w+')
-Marshal.dump(@recipes, file)
+file = File.open("List", 'w+')
+Marshal.dump(grocery, file)
 file.pos = 0
-recipe = Marshal.load(file)
+grocery = Marshal.load(file)
